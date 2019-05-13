29975713718a42ba6c1a408801fe1c8d12e0f1e5
zhanghengwei <zhanghengwei06837@hellobike.com>
2019-05-13 15:37:56 +0800

    add git ignore

---
+++ Practice/src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: zhanghengwei
+ * @CreateTime: 2019-05-09 18:23
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
